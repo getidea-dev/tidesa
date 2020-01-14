154e0ec2e3ba9eedd509e4c887d752f34816f9fd
oneight2 <1606109@sttgarut.ac.id>
2020-01-14 11:37:40 +0700

    tes dropzone

--- application/views/auth/register.php
+++ application/views/auth/register.php
@@ -7,7 +7,7 @@
                     <img src="../assets/img/brand/white.png" />
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse-main" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbar-collapse-main">
                     <!-- Collapse header -->
@@ -20,13 +20,12 @@
                             </div>
                             <div class="col-6 collapse-close">
                                 <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse-main" aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle sidenav">
-                                    <span></span>
-                                    <span></span>
+                                <span></span>
+                                <span></span>
                                 </button>
                             </div>
                         </div>
                     </div>
-
                 </div>
             </div>
         </nav>
@@ -44,97 +43,101 @@
             </div>
             <div class="separator separator-bottom separator-skew zindex-100">
                 <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                    <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
-                </svg>
-            </div>
+                <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
+            </svg>
         </div>
-        <!-- Page content -->
-        <div class="container mt--8 pb-5">
-            <!-- Table -->
-            <div class="row justify-content-center">
-                <div class="col-lg-6 col-md-8">
-                    <div class="card bg-secondary shadow border-0">
-                        <div class="card-body px-lg-5 py-lg-5">
-                            <div class="text-center text-muted mb-4">
-                                <small>Or sign up with credentials</small>
-                            </div>
-                            <?= $this->session->flashdata('message'); ?>
-                            <form role="form" method="post" action="<?= base_url('auth/register') ?>">
-                                <div class="form-group">
-                                    <div class="input-group input-group-alternative mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text"><i class="ni ni-hat-3"></i></span>
-                                        </div>
-                                        <input class="form-control" placeholder="Nama Lengkap" name="nama_lengkap" type="text">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="input-group input-group-alternative mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text"><i class="ni ni-email-83"></i></span>
-                                        </div>
-                                        <input class="form-control" placeholder="Email" name="email" type="email">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="input-group input-group-alternative mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text"><i class="ni ni-email-83"></i></span>
-                                        </div>
-                                        <input class="form-control" placeholder="Tempat Lahir" name="tempat_lahir" type="text">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="input-group input-group-alternative mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text"><i class="ni ni-email-83"></i></span>
-                                        </div>
-                                        <input class="form-control" placeholder="Tanggal Lahir" type="date" name="tanggal_lahir">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="input-group input-group-alternative mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text"><i class="ni ni-email-83"></i></span>
-                                        </div>
-                                        <input class="form-control" placeholder="Nomor Telepon" type="text" name="no_telepon">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="input-group input-group-alternative mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text"><i class="ni ni-email-83"></i></span>
-                                        </div>
-                                        <input class="form-control" placeholder="File SK" type="file" name="file">
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <div class="input-group input-group-alternative mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text"><i class="ni ni-email-83"></i></span>
-                                        </div>
-                                        <input class="form-control" placeholder="Nama Desa" type="text" name="nama_desa">
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <div class="input-group input-group-alternative mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text"><i class="ni ni-email-83"></i></span>
-                                        </div>
-                                        <input class="form-control" placeholder="Subdomain desa" type="text" id="subdomain" name="domain">
-                                        <p id="notif"></p>
-                                    </div>
-                                </div>
-
-                                <div class="text-center">
-                                    <button type="submit" class="btn btn-primary mt-4">DAFTAR</button>
-                                </div>
-                            </form>
+    </div>
+    <!-- Page content -->
+    <div class="container mt--8 pb-5">
+        <!-- Table -->
+        <div class="row justify-content-center">
+            <div class="col-lg-6 col-md-8">
+                <div class="card bg-secondary shadow border-0">
+                    <div class="card-body px-lg-5 py-lg-5">
+                        <div class="text-center text-muted mb-4">
+                            <small>Or sign up with credentials</small>
                         </div>
+                        <?= $this->session->flashdata('message'); ?>
+                        <form role="form" method="post" action="<?= base_url('auth/register') ?>">
+                            <div class="form-group">
+                                <div class="input-group input-group-alternative mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="ni ni-hat-3"></i></span>
+                                    </div>
+                                    <input class="form-control" placeholder="Nama Lengkap" name="nama_lengkap" type="text">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="input-group input-group-alternative mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                                    </div>
+                                    <input class="form-control" placeholder="Email" name="email" type="email">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="input-group input-group-alternative mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                                    </div>
+                                    <input class="form-control" placeholder="Tempat Lahir" name="tempat_lahir" type="text">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="input-group input-group-alternative mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                                    </div>
+                                    <input class="form-control" placeholder="Tanggal Lahir" type="date" name="tanggal_lahir">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="input-group input-group-alternative mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                                    </div>
+                                    <input class="form-control" placeholder="Nomor Telepon" type="text" name="no_telepon">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="dropzone dropzone-single mb-3" data-toggle="dropzone" data-dropzone-url="http://">
+                                    <div class="fallback">
+                                        <div class="custom-file">
+                                            <input type="file" class="custom-file-input" id="projectCoverUploads">
+                                            <label class="custom-file-label" for="projectCoverUploads">Choose file</label>
+                                        </div>
+                                    </div>
+                                    <div class="dz-preview dz-preview-single">
+                                        <div class="dz-preview-cover">
+                                            <img class="dz-preview-img" src="...html" alt="..." data-dz-thumbnail>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="input-group input-group-alternative mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                                    </div>
+                                    <input class="form-control" placeholder="Nama Desa" type="text" name="nama_desa">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="input-group input-group-alternative mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                                    </div>
+                                    <input class="form-control" placeholder="Subdomain desa" type="text" id="subdomain" name="domain">
+                                    <p id="notif"></p>
+                                </div>
+                            </div>
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-primary mt-4">DAFTAR</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -7,58 +7,39 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <title>404 Page Not Found</title>
 <style type="text/css">
 
-::selection { background-color: #E13300; color: white; }
-::-moz-selection { background-color: #E13300; color: white; }
-
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
+body{
+  background: white;
+  text-align: center;
+  font-family: 'Droid Sans', sans-serif;
 }
-
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
+img{
+	width: 100%;
 }
-
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
+h1{
+  margin-top: 30px;
+  color: black;
+  text-shadow: 0px 2px 0px rgba(150, 150, 150, 1);
 }
-
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
+#err-icon{
+  font-size: 80pt;
+  color: #bdc3c7;
+  margin-top: 150px;
+  text-shadow: 0px 3px 0px rgba(150, 150, 150, 1);
+  -webkit-transition: 0.5s;
 }
-
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	box-shadow: 0 0 8px #D0D0D0;
+#err-icon:hover{
+  -webkit-transform: scale(1.1);
+  color: #e74c3c;
+  text-shadow: 0px 3px 0px rgba(192, 57, 43, 1);
 }
-
-p {
-	margin: 12px 15px 12px 15px;
+p{
+  color: rgba(189, 195, 199,1.0);
+  font-weight: bold;
 }
 </style>
 </head>
-<body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
-	</div>
-</body>
+
 </html>
+
+<h1><?php echo $heading; ?></h1>
+<p><?php echo $message; ?></p>
