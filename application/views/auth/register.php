169c2c904785661de222d6e711883572e2c58f4f
oneight2 <1606109@sttgarut.ac.id>
2020-01-14 13:24:52 +0700

    layout

--- application/views/auth/register.php
+++ application/views/auth/register.php
@@ -48,10 +48,10 @@
         </div>
     </div>
     <!-- Page content -->
-    <div class="container mt--8 pb-5">
+    <div class="container mt--8 pb-2">
         <!-- Table -->
         <div class="row justify-content-center">
-            <div class="col-lg-6 col-md-8">
+            <div class="col ">
                 <div class="card bg-secondary shadow border-0">
                     <div class="card-body px-lg-5 py-lg-5">
                         <div class="text-center text-muted mb-4">
@@ -59,110 +59,118 @@
                         </div>
                         <?= $this->session->flashdata('message'); ?>
                         <form role="form" method="post" action="<?= base_url('auth/register') ?>">
-                            <div class="form-group">
-                                <div class="input-group input-group-alternative mb-3">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="ni ni-hat-3"></i></span>
-                                    </div>
-                                    <input class="form-control" placeholder="Nama Lengkap" name="nama_lengkap" type="text">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="input-group input-group-alternative mb-3">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="ni ni-email-83"></i></span>
-                                    </div>
-                                    <input class="form-control" placeholder="Email" name="email" type="email">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="input-group input-group-alternative mb-3">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="ni ni-email-83"></i></span>
-                                    </div>
-                                    <input class="form-control" placeholder="Tempat Lahir" name="tempat_lahir" type="text">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="input-group input-group-alternative mb-3">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="ni ni-email-83"></i></span>
-                                    </div>
-                                    <input class="form-control" placeholder="Tanggal Lahir" type="date" name="tanggal_lahir">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="input-group input-group-alternative mb-3">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="ni ni-email-83"></i></span>
-                                    </div>
-                                    <input class="form-control" placeholder="Nomor Telepon" type="text" name="no_telepon">
-                                </div>
-                            </div>
-                            <!-- cik dropzone -->
-                            <h5>Upload FILE SK</h5>
-                            <div class="dropzone dropzone-multiple" data-toggle="dropzone" data-dropzone-multiple data-dropzone-url="http://">
-                                <div class="fallback">
-                                    <div class="custom-file">
-                                        <input type="file" class="custom-file-input" id="customFileUploadMultiple" multiple>
-                                        <label class="custom-file-label" for="customFileUploadMultiple">Choose file</label>
-                                    </div>
-                                </div>
-                                <ul class="dz-preview dz-preview-multiple list-group list-group-lg list-group-flush">
-                                    <li class="list-group-item px-0">
-                                        <div class="row align-items-center">
-                                            <div class="col-auto">
-                                                <div class="avatar">
-                                                    <img class="avatar-img rounded" src="...html" alt="..." data-dz-thumbnail>
-                                                </div>
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <div class="input-group input-group-alternative mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="ni ni-hat-3"></i></span>
                                             </div>
-                                            <div class="col ml--3">
-                                                <h4 class="mb-1" data-dz-name>...</h4>
-                                                <p class="small text-muted mb-0" data-dz-size>...</p>
+                                            <input class="form-control" placeholder="Nama Lengkap" name="nama_lengkap" type="text">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group input-group-alternative mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="ni ni-email-83"></i></span>
                                             </div>
-                                            <div class="col-auto">
-                                                <div class="dropdown">
-                                                    <a href="#" class="dropdown-ellipses dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                        <i class="fe fe-more-vertical"></i>
-                                                    </a>
-                                                    <div class="dropdown-menu dropdown-menu-right">
-                                                        <a href="#" class="dropdown-item" data-dz-remove>
-                                                            Remove
-                                                        </a>
-                                                    </div>
-                                                </div>
+                                            <input class="form-control" placeholder="Email" name="email" type="email">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group input-group-alternative mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                                            </div>
+                                            <input class="form-control" placeholder="Tempat Lahir" name="tempat_lahir" type="text">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group input-group-alternative mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                                            </div>
+                                            <input class="form-control" placeholder="Tanggal Lahir" type="date" name="tanggal_lahir">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group input-group-alternative mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                                            </div>
+                                            <input class="form-control" placeholder="Nomor Telepon" type="text" name="no_telepon">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <!-- cik dropzone -->
+                                    <h5>Upload FILE SK</h5>
+                                    <div class="dropzone dropzone-multiple" data-toggle="dropzone" data-dropzone-multiple data-dropzone-url="http://">
+                                        <div class="fallback">
+                                            <div class="custom-file">
+                                                <input type="file" class="custom-file-input" id="customFileUploadMultiple" multiple>
+                                                <label class="custom-file-label" for="customFileUploadMultiple">Choose file</label>
                                             </div>
                                         </div>
-                                    </li>
-                                </ul>
-                            </div>
-                            <br>
-                            <!-- stop dropzone -->
-                            
-                            
-                            <div class="form-group">
-                                <div class="input-group input-group-alternative mb-3">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                                        <ul class="dz-preview dz-preview-multiple list-group list-group-lg list-group-flush">
+                                            <li class="list-group-item px-0">
+                                                <div class="row align-items-center">
+                                                    <div class="col-auto">
+                                                        <div class="avatar">
+                                                            <img class="avatar-img rounded" src="...html" alt="..." data-dz-thumbnail>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col ml--3">
+                                                        <h4 class="mb-1" data-dz-name>...</h4>
+                                                        <p class="small text-muted mb-0" data-dz-size>...</p>
+                                                    </div>
+                                                    <div class="col-auto">
+                                                        <div class="dropdown">
+                                                            <a href="#" class="dropdown-ellipses dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                                <i class="fe fe-more-vertical"></i>
+                                                            </a>
+                                                            <div class="dropdown-menu dropdown-menu-right">
+                                                                <a href="#" class="dropdown-item" data-dz-remove>
+                                                                    Remove
+                                                                </a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        </ul>
                                     </div>
-                                    <input class="form-control" placeholder="Subdomain desa" type="text" id="subdomain" name="subdomain">
-                                    <p id="notif"></p>
+                                    <br>
+                                    <!-- stop dropzone -->
+                                    
+                                    
+                                    <div class="form-group">
+                                        <div class="input-group input-group-alternative mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                                            </div>
+                                            <input class="form-control" placeholder="Subdomain desa" type="text" id="subdomain" name="subdomain">
+                                            <p id="notif"></p>
+                                        </div>
+                                        <input class="form-control" placeholder="Nama Desa" type="text" name="nama_desa">
+                                    </div>
+                                    
+                                    <div class="form-group">
+                                        <div class="input-group input-group-alternative mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                                            </div>
+                                            <input class="form-control" placeholder="Subdomain desa" type="text" id="subdomain" name="domain">
+                                            <p id="notif"></p>
+                                        </div>
+                                    </div>
+                                    <div class="text-center">
+                                        <button type="submit" class="btn btn-primary mt-4">DAFTAR</button>
+                                    </div>
+                                    
                                 </div>
-                                <input class="form-control" placeholder="Nama Desa" type="text" name="nama_desa">
                             </div>
                             
-                            <div class="form-group">
-                                <div class="input-group input-group-alternative mb-3">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="ni ni-email-83"></i></span>
-                                    </div>
-                                    <input class="form-control" placeholder="Subdomain desa" type="text" id="subdomain" name="domain">
-                                    <p id="notif"></p>
-                                </div>
-                            </div>
-                            <div class="text-center">
-                                <button type="submit" class="btn btn-primary mt-4">DAFTAR</button>
-                            </div>
                         </form>
                     </div>
                 </div>
